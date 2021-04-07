eddae67a267e681a1517847cdc7b626595288698
sangeethasr <sangeetharajan567@gmail.com>
2021-04-08 01:59:37 +0530

    Added Javascript File

---
+++ app.js
@@ -0,0 +1,43 @@
+
+function domainFunction(){
+   var a = document.getElementById("domains");
+   var e = document.getElementById("contactus");
+   var f = document.getElementById("signin");
+
+
+   if( a.style.display === "none"){
+       a.style.display = "flex";
+   }else{
+        a.style.display = "none";
+   }
+}
+
+function hostingFunction(){
+    var b = document.getElementById("hosting");
+
+    if( b.style.display === "none"){
+        b.style.display = "flex";
+    }else{
+         b.style.display = "none";
+    }
+}
+
+function securityFunction(){
+    var c = document.getElementById("security");
+
+    if( c.style.display === "none"){
+        c.style.display = "flex";
+    }else{
+        c.style.display = "none";
+    }
+}
+
+function marketingFunction(){
+    var d = document.getElementById("Marketing");
+
+    if( d.style.display === "none"){
+        d.style.display = "flex";
+    }else{
+        d.style.display = "none";
+    }
+}
